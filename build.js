cda4867b275b6b491913e5a872ccae8e63397958
Johannes Stein <johannesstein@freeze-dev.com>
2017-05-16 10:06:55 +0200

    Update scripts

---
+++ build.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const FOLDER_PREFIX = './files';
+
+const folders = fs.readdirSync(FOLDER_PREFIX)
+  .filter(file => file.indexOf('.') < 0)
+  .filter(folder => {
+    try {
+      const stats = fs.lstatSync(`${FOLDER_PREFIX}/${folder}/package.json`);
+
+      return stats.isFile();
+    } catch(e) {
+      return false;
+    }
+  })
+  .forEach(folder => {
+    cd(`${FOLDER_PREFIX}/${folder}`);
+    exec('yarn');
+    exec('yarn build');
+    cd('..');
+  })
