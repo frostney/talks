782e7babd39878abf32cfbc5b47dfa6fdb6ba611
Johannes Stein <johannesstein@freeze-dev.com>
2017-09-09 22:41:28 +0300

    Updates

--- files/monorepo-reactnext/presentation/index.js
+++ files/monorepo-reactnext/presentation/index.js
@@ -34,10 +34,6 @@ require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 const images = {
-  mythbusters: require("../assets/MythBusters.jpg"),
-  reddit: require("../assets/reddit.png"),
-  cyclops: require("../assets/cyclops.gif"),
-  ants: require("../assets/ants.gif"),
   lerna: require("../assets/lerna.png"),
   yarn: require("../assets/yarn.svg"),
   title: require("../assets/title.jpg"),
@@ -46,7 +42,10 @@ const images = {
   step3: require("../assets/step3.png"),
   step4: require("../assets/step4.png"),
   step5: require("../assets/step5.png"),
-  step6: require("../assets/step6.png")
+  step6: require("../assets/step6.png"),
+  benefits: require("../assets/benefits.jpg"),
+  challenges: require("../assets/challenges.jpg"),
+  madscience: require("../assets/madscience.png")
 };
 
 preloader(images);
@@ -60,21 +59,6 @@ const theme = createTheme({
   primary: "Montserrat",
   secondary: "Montserrat"
 });
-
-class SlideWithTitle extends React.Component {
-  render() {
-    const { size = 2, title, children } = this.props;
-
-    return (
-      <Slide>
-        <Heading size={size} textColor="secondary" style={{ paddingBottom: 40 }}>
-          {title}
-        </Heading>
-        {children}
-      </Slide>
-    );
-  }
-}
 
 export default class Presentation extends React.Component {
   render() {
@@ -92,22 +76,28 @@ export default class Presentation extends React.Component {
               </Fill>
             </Layout>
         </Slide>
-        <SlideWithTitle title="🖐" />
-        <SlideWithTitle title="What is a monorepo?">
-          <Link href="https://developer.atlassian.com/blog/2015/10/monorepos-in-git/" textColor="secondary">developer.atlassian.com/blog/2015/10/monorepos-in-git/</Link>
-        </SlideWithTitle>
         <Slide>
-          <Heading size={4} textColor="secondary">
+          <Heading size={2} textColor="secondary">🖐</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={2} textColor="secondary">What is a monorepo?</Heading>
+          <Link href="https://developer.atlassian.com/blog/2015/10/monorepos-in-git/" textColor="secondary">developer.atlassian.com/blog/2015/10/monorepos-in-git/</Link>
+          </Slide>
+        <Slide>
+          <Heading size={3} textColor="secondary">
             The repository contains more than one logical project
           </Heading>
         </Slide>
         <Slide>
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             These projects are most likely unrelated, loosely connected or can be connected by other means
           </Heading>
         </Slide>
-        <Slide>
-          Mad science?
+        <Slide bgImage={images.madscience} bgDarken={0.5}>
+          <BlockQuote>
+            <Quote textColor="secondary">Multiple repositories? Where we're going, we don't need... multiple repositories.</Quote>
+            <Cite>Doc Brown (Back To The Future)</Cite>
+          </BlockQuote>
         </Slide>
         <Slide bgColor="secondary" textColor="primary">
           <BlockQuote>
@@ -124,7 +114,7 @@ export default class Presentation extends React.Component {
           </Appear>
         </Slide>
         <Slide>
-          <Heading size={2} textColor="secondary" style={{ paddingBottom: 20 }}>
+          <Heading size={3} textColor="secondary" style={{ paddingBottom: 20 }}>
             Imagine this project growing over time
           </Heading>
         </Slide>
@@ -153,54 +143,62 @@ export default class Presentation extends React.Component {
           <CodePane source={require("raw-loader!../assets/monorepo.example")} />
         </Slide>
         <Slide>
-          <Heading size={2}>It's just a bunch of folders</Heading>
+          <Heading size={3} textColor="secondary">It's just a bunch of folders</Heading>
         </Slide>
-        <Slide bgColor="primary">
+        <Slide bgImage={images.benefits} bgDarken={0.45}>
           <Heading size={2} textColor="secondary">
             Benefits
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Single lint, build, test and release process
           </Heading>
         </Slide>
-        <Slide bgColor="primary">
-          <List>
-            <Appear>
-              <ListItem>ESLint</ListItem>
-            </Appear>
-            <Appear>
-              <ListItem>Test framework setup</ListItem>
-            </Appear>
-          </List>
+        <Slide>
+          <Heading size={4} textColor="secondary" style={{ paddingBottom: 40 }}>
+            Decentralized ESLint files...
+          </Heading>
+          <CodePane source={require("raw-loader!../assets/polyrepo.eslint.example")} />
+        </Slide>
+        <Slide>
+          <Heading size={4} textColor="secondary" style={{ paddingBottom: 40 }}>
+            ...become centralized
+          </Heading>
+          <CodePane source={require("raw-loader!../assets/monorepo.eslint.example")} />
+        </Slide>
+        <Slide>
+          <Heading size={4} textColor="secondary" style={{ paddingBottom: 40 }}>
+            Extend from centralized if necessary
+          </Heading>
+          <CodePane lang="json" source={require("raw-loader!../assets/eslintrc.example")} />
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Easier to set up the development environment
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Easier to coordinate changes across the codebase
           </Heading>
         </Slide>
         <Slide>
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Removes dependencies on code reviews
           </Heading>
         </Slide>
         <Slide>
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Removes the need to release common pieces of code
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
-            Easier to test modules
+          <Heading size={3} textColor="secondary">
+            Simplifies end-to-end testing
           </Heading>
         </Slide>
-        <Slide bgColor="primary">
+        <Slide bgImage={images.challenges} bgDarken={0.45}>
           <Heading size={2} textColor="secondary">
             Challenges
           </Heading>
@@ -258,7 +256,7 @@ export default class Presentation extends React.Component {
           <CodePane lang="jsx" source={require("raw-loader!../assets/ci.example")} />
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Synchronization with Open Source repositories
           </Heading>
         </Slide>
@@ -268,12 +266,12 @@ export default class Presentation extends React.Component {
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Keeping certain modules independant
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             Ownership
           </Heading>
         </Slide>
@@ -283,11 +281,11 @@ export default class Presentation extends React.Component {
           </Heading>
         </Slide>
         <Slide bgColor="primary">
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             It's just a bunch of folders
           </Heading>
           <Appear>
-          <Heading size={4} textColor="secondary">
+          <Heading size={3} textColor="secondary">
             We need a tool to run the same command across multiple folders
           </Heading>
           </Appear>
@@ -302,17 +300,17 @@ export default class Presentation extends React.Component {
           </BlockQuote>
         </Slide>
         <Slide>
-          <Heading size={2} textColor="secondary" style={{ paddingBottom: 40, lineHeight: 0.5 }}>
+          <Heading size={3} textColor="secondary" style={{ paddingBottom: 40, lineHeight: 0.5 }}>
             <Code textColor="secondary">yarn add global lerna</Code>
           </Heading>
           <Appear>
-            <Heading size={4} textColor="secondary" style={{ lineHeight: 0.75 }}>
+            <Heading size={3} textColor="secondary" style={{ lineHeight: 0.75 }}>
               (<Code textColor="secondary">npm install -g lerna</Code>)
             </Heading>
           </Appear>
         </Slide>
         <Slide>
-          <Heading size={2} textColor="secondary">Let's create a monorepo</Heading>
+          <Heading size={3} textColor="secondary">Let's create a monorepo</Heading>
           <Terminal title="" output={[
             "lerna init",
             `├── lerna-debug.log
@@ -357,11 +355,11 @@ export default class Presentation extends React.Component {
           />
         </Slide>
         <Slide>
-          <Heading size={2}><Code>lerna.json</Code></Heading>
+          <Heading size={3}><Code>lerna.json</Code></Heading>
           <CodePane lang="json" source={require("raw-loader!../assets/lerna.json.example")} />
         </Slide>
         <Slide>
-          <Heading size={2}>Bootstrap</Heading>
+          <Heading size={3}>Bootstrap</Heading>
         </Slide>
         <Slide bgColor="primary">
           <Heading size={4} textColor="secondary">
@@ -374,13 +372,16 @@ export default class Presentation extends React.Component {
           </Heading>
         </Slide>
         <Slide>
-          <Heading size={2}>Running scripts</Heading>
+          <Heading size={3}>Running scripts</Heading>
         </Slide>
         <Slide>
-          <Heading size={2}>Releases</Heading>
+          <Heading size={3}>Releases</Heading>
         </Slide>
         <Slide>
-          <Heading size={2}>Import repositories</Heading>
+          <Heading size={4}>Independant release</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={3}>Import repositories</Heading>
         </Slide>
         <Slide bgColor="primary">
           <Image src={images.yarn} />
