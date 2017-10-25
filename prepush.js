2eeaaaa8d6cfabc44c679847effd90c0dc02183d
Johannes Stein <johannesstein@freeze-dev.com>
2017-10-25 22:51:14 +0200

    Update Github pages

--- prepush.js
+++ prepush.js
@@ -1,6 +1,15 @@
-exec('yarn clean');
-exec('yarn copy');
-exec('git add .');
-if (exec('git commit -m "Update Github pages"').code !== 0) {
-  exit(0);
+const commitMessage = 'Update Github pages';
+
+// If last commit did already update Github pages, no need to run everything again
+const lastCommit = exec('git log --format=%B -n 1 HEAD', { silent: true }).replace(/\n/g, '');
+
+if (lastCommit !== commitMessage) {
+  exec('yarn clean');
+  exec('yarn copy');
+  exec('git add .');
+  if (exec(`git commit -m "${commitMessage}"`).code !== 0) {
+    exit(0);
+  }
+} else {
+  console.log('Nothing to do.');
 }
