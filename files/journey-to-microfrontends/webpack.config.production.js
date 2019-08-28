a232bafe6bef01893ca8d5e5b755859ce0458164
Johannes Stein <johannesstein@freeze-dev.com>
2019-08-28 13:33:53 +0100

    Image fix

--- files/journey-to-microfrontends/webpack.config.production.js
+++ files/journey-to-microfrontends/webpack.config.production.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "dist/"
   },
   plugins: [
     new webpack.DefinePlugin({
